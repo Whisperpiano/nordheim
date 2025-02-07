cce624b0ac7346781b1bf224d303bca9a2299d30
Jesús Alberola Herrero <jesusalberola90@gmail.com>
2025-02-07 07:03:11 +0100

    downgrade to tailwind 3

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import { router } from "./router/routes";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Toaster } from "sonner";
+import { HeroUIProvider } from "@heroui/system";
 
 import "./index.css";
 
@@ -15,7 +16,9 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <ReactQueryDevtools initialIsOpen={false} />
-      <RouterProvider router={router} />
+      <HeroUIProvider>
+        <RouterProvider router={router} />
+      </HeroUIProvider>
       <Toaster richColors expand={false} position="bottom-right" />
     </QueryClientProvider>
   </StrictMode>
