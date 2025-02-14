f1831e261bfe4891e7df98e08696906c85906c20
Jesús Alberola Herrero <jesusalberola90@gmail.com>
2025-02-14 10:05:01 +0100

    fixed problem with overflow

--- src/layout/components/BrandValues/BrandValues.component.tsx
+++ src/layout/components/BrandValues/BrandValues.component.tsx
@@ -2,18 +2,20 @@ import { brandValues } from "../../../constants";
 
 export default function BrandValues() {
   return (
-    <section className="flex text-center border-y border-neutral-300 py-10 mt-3">
-      {brandValues.map(({ value, descripton, icon: Icon }, i) => (
-        <div className="p-10" key={i}>
-          <Icon size={28} className="text-neutral-950/50 mx-auto mb-5" />
-          <h3 className="font-condensed font-medium text-neutral-950/75 uppercase text-sm mb-3">
-            {value}
-          </h3>
-          <p className="text-neutral-950 font-sans text-xs font-normal">
-            {descripton}
-          </p>
-        </div>
-      ))}
+    <section className="border-y border-neutral-300 py-10 mt-3">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 text-center max-w-[1440px] mx-auto">
+        {brandValues.map(({ value, descripton, icon: Icon }, i) => (
+          <div className="p-8" key={i}>
+            <Icon size={28} className="text-neutral-950/50 mx-auto mb-5" />
+            <h3 className="font-condensed font-medium text-neutral-950/75 uppercase text-sm mb-3">
+              {value}
+            </h3>
+            <p className="text-neutral-950 font-sans text-xs font-normal">
+              {descripton}
+            </p>
+          </div>
+        ))}
+      </div>
     </section>
   );
 }
