8bf76eba4b317eaa6270d9e7174d39feb20adb71
Jesús Alberola Herrero <jesusalberola90@gmail.com>
2025-02-14 08:58:40 +0100

    bg

--- src/pages/Products/components/Banner/Banner.component.tsx
+++ src/pages/Products/components/Banner/Banner.component.tsx
@@ -2,8 +2,11 @@ import { categories } from "../../../../constants";
 import { BannerProps } from "./Banner.types";
 
 export default function Banner({ category }: BannerProps) {
+  console.log(category);
   return (
-    <section className="bg-center bg-no-repeat bg-cover bg-[url('/city-products.jpg')] bg-black/25 bg-blend-multiply h-[650px] flex items-end">
+    <section
+      className={`bg-center bg-no-repeat bg-cover bg-[url('https://ppufwgcofnfrgdeqxesf.supabase.co/storage/v1/object/public/banner-images//${category}-banner-1024.webp')] lg:bg-[url('https://ppufwgcofnfrgdeqxesf.supabase.co/storage/v1/object/public/banner-images//${category}-banner-2560.webp')] bg-black/25 bg-blend-multiply h-[400px] lg:h-[650px] flex items-end`}
+    >
       <div className="px-4 mb-16">
         <h1 className="mb-2 text-6xl font-semibold tracking-tight leading-none text-neutral-50 uppercase font-condensed ">
           {categories[category].title}
