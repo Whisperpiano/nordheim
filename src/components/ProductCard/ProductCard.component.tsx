d4c173dfbcd66f073abe275dcd2da16b2ebf59cd
Jesús Alberola Herrero <jesusalberola90@gmail.com>
2025-02-17 15:00:23 +0100

    fixed small bug with image

--- src/components/ProductCard/ProductCard.component.tsx
+++ src/components/ProductCard/ProductCard.component.tsx
@@ -34,7 +34,7 @@ export default function ProductCard({ product }: { product: FullProduct }) {
               type="image/webp"
             />
             <motion.img
-              src={`https://ppufwgcofnfrgdeqxesf.supabase.co/storage/v1/object/public/${product.category}-images//${product.slug}.jpeg`}
+              src={`https://ppufwgcofnfrgdeqxesf.supabase.co/storage/v1/object/public/${product.category}-images//${product.slug}.jpg`}
               alt="City"
               className="object-cover aspect-[4/5] w-full group-hover:scale-105 transition-transform duration-700 blur-xl"
               style={{
