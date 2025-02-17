35c5ce53108ae782622f6d33057703b44be71068
Jesús Alberola Herrero <jesusalberola90@gmail.com>
2025-02-17 15:18:04 +0100

    add span to cart

--- src/layout/components/Header/SecondaryNav/SecondaryNav.component.tsx
+++ src/layout/components/Header/SecondaryNav/SecondaryNav.component.tsx
@@ -1,18 +1,21 @@
 import {
-  RiHandbagLine,
   RiMenu3Line,
   RiSearchLine,
+  RiShoppingBag2Line,
   RiUserLine,
 } from "react-icons/ri";
 import { Link } from "react-router";
 import { useModalStore } from "../../../../store/modalStore";
 import Button from "../../../../components/Button/Button.component";
 import buttonVariants from "../../../../components/Button/Button.styles";
+import { useCartStore } from "../../../../store/cartStore";
 
 export default function SecondaryNav() {
   const setCartOpen = useModalStore((state) => state.setCartOpen);
   const setSearchOpen = useModalStore((state) => state.setSearchOpen);
   const setMobileMenuOpen = useModalStore((state) => state.setMobileMenuOpen);
+
+  const cart = useCartStore((state) => state.cart);
 
   return (
     <nav className="flex text-neutral-500" aria-label="Secondary navigation">
@@ -41,15 +44,19 @@ export default function SecondaryNav() {
             <RiUserLine size={20} />
           </Link>
         </li>
-        <li className="flex">
+        <li className="flex group">
           <Button
             variant="default"
             size="sm"
             shape="rounded"
             onClick={() => setCartOpen(true)}
             aria-label="Open cart"
+            className="relative"
           >
-            <RiHandbagLine size={20} />
+            <RiShoppingBag2Line size={20} />
+            {cart.length > 0 && (
+              <span className="top-1.5 right-1.5 absolute text-xs font-bold font-sans bg-neutral-500 h-2 w-2 rounded-full group-hover:bg-neutral-700 transition-colors duration-300"></span>
+            )}
           </Button>
         </li>
         <li className="flex lg:hidden ">
