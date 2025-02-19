c96b99c666929b50c84f92f3e808d838334cd4b2
Jesús Alberola Herrero <jesusalberola90@gmail.com>
2025-02-19 14:34:17 +0100

    added effect to sort

--- src/pages/Products/Mountain/Mountain.page.tsx
+++ src/pages/Products/Mountain/Mountain.page.tsx
@@ -2,10 +2,15 @@ import Banner from "../components/Banner/Banner.component";
 import FiltersBar from "../../../components/FiltersBar/FiltersBar.component";
 import ProductCard from "../../../components/ProductCard/ProductCard.component";
 import { useQuery } from "@tanstack/react-query";
-import { useEffect } from "react";
+import { useEffect, useMemo } from "react";
 import { getProductsByCategory } from "../../../api/products/products";
+import { useSearchParams } from "react-router";
+import { AnimatePresence, motion } from "framer-motion";
 
 export default function Mountain() {
+  const [searchParams] = useSearchParams();
+  const sort = searchParams.get("sort");
+
   const {
     data: products,
     isError,
@@ -23,19 +28,47 @@ export default function Mountain() {
     }
   }, [products, refetch]);
 
+  const sortedProducts = useMemo(() => {
+    if (!products) return [];
+
+    return [...products].sort((a, b) => {
+      if (sort === "title-asc") return a.title.localeCompare(b.title);
+      if (sort === "title-desc") return b.title.localeCompare(a.title);
+      if (sort === "price-asc") return a.price - b.price;
+      if (sort === "price-desc") return b.price - a.price;
+      return 0;
+    });
+  }, [products, sort]);
+
   return (
     <>
-      <Banner category="mountain" />
-      <FiltersBar />
-      <section className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-0.5">
+      <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
+        <Banner category="mountain" />
+        <FiltersBar />
+      </motion.div>
+      <motion.section
+        className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-0.5"
+        layout
+      >
         {isError && <div className="min-h-screen">Error</div>}
         {!products && <div className="min-h-screen">Loading</div>}
-        {products &&
-          !isError &&
-          products.map((product, i) => (
-            <ProductCard key={i} product={product} />
-          ))}
-      </section>
+        <AnimatePresence>
+          {products &&
+            !isError &&
+            sortedProducts.map((product) => (
+              <motion.div
+                key={product.id}
+                layout
+                initial={{ opacity: 0, y: 20 }}
+                animate={{ opacity: 1, y: 0 }}
+                exit={{ opacity: 0 }}
+                transition={{ duration: 0.3 }}
+              >
+                <ProductCard product={product} />
+              </motion.div>
+            ))}
+        </AnimatePresence>
+      </motion.section>
     </>
   );
 }
