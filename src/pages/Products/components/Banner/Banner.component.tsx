8808eefc0b5bf3ea3f09e7ff8840017dd531417d
Jesús Alberola Herrero <jesusalberola90@gmail.com>
2025-02-26 10:57:25 +0100

    responsive banner

--- src/pages/Products/components/Banner/Banner.component.tsx
+++ src/pages/Products/components/Banner/Banner.component.tsx
@@ -5,11 +5,11 @@ import ImageBanner from "./components/ImageBanner/ImageBanner.component";
 export default function Banner({ category }: BannerProps) {
   return (
     <section className={`relative h-[400px] lg:h-[650px] flex items-end`}>
-      <div className="px-4 mb-16 absolute w-full h-ful z-20">
-        <h1 className="mb-2 text-6xl font-semibold tracking-tight leading-none text-neutral-50 uppercase font-condensed ">
+      <div className="px-4 sm:px-8 lg:px-12 xl:px-16 pb-8 lg:pb-12 xl:mb-16 absolute w-full z-20">
+        <h1 className="mb-1 sm:mb-2 text-4xl sm:text-6xl font-semibold tracking-tight leading-none text-neutral-50 uppercase font-condensed ">
           {categories[category].title}
         </h1>
-        <p className="text-lg font-normal text-neutral-50 font-sans">
+        <p className="ml-1 text-sm sm:text-lg font-normal text-neutral-50 font-sans">
           {categories[category].description}
         </p>
       </div>
