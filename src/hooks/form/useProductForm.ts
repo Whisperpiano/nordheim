e1d16733b266ed4d10b377ee022b2fcf7f057e16
Jesús Alberola Herrero <jesusalberola90@gmail.com>
2025-02-26 14:29:14 +0100

    create useproductform hook

--- src/hooks/form/useProductForm.ts
+++ src/hooks/form/useProductForm.ts
@@ -1,17 +1,54 @@
-// import { useState } from "react";
+import { useState } from "react";
+import { useModalStore } from "../../store/modalStore";
+import { useCartStore } from "../../store/cartStore";
+import { VariantsArray } from "../../lib/schemas/productSchema";
 
-// export function useProductForm({ stock }: { stock: number }) {
-//   const [quantity, setQuantity] = useState(1);
+interface ProductFormHookProps {
+  variants: VariantsArray;
+  title: string;
+  slug: string;
+  price: number;
+  category: "city" | "mountain";
+}
 
-//   const handleDecrement = () => {
-//     if (quantity <= 1) return;
-//     setQuantity(quantity - 1);
-//   };
+export function useProductForm({
+  variants,
+  title,
+  slug,
+  price,
+  category,
+}: ProductFormHookProps) {
+  const [quantity, setQuantity] = useState(1);
 
-//   const handleIncrement = () => {
-//     if (quantity === stock) return;
-//     setQuantity(quantity + 1);
-//   };
+  const setCartOpen = useModalStore((state) => state.setCartOpen);
+  const addItemToCart = useCartStore((state) => state.addItem);
 
-//   return { setQuantity, quantity, handleDecrement, handleIncrement };
-// }
+  const handleDecrement = () => {
+    if (quantity <= 1) return;
+    setQuantity(quantity - 1);
+  };
+
+  const handleIncrement = () => {
+    setQuantity(quantity + 1);
+  };
+
+  const handleAddToCart = () => {
+    setCartOpen(true);
+    addItemToCart({
+      id: variants[0].product_id,
+      title,
+      slug,
+      price,
+      category,
+      quantity,
+      variant: {
+        id: variants[0].id,
+        volume: variants[0].volume,
+        color: variants[0].color,
+      },
+    });
+    setQuantity(1);
+  };
+
+  return { quantity, handleDecrement, handleIncrement, handleAddToCart };
+}
