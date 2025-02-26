7190a0e2c8217e8cc5a707fc2d311d7857d263b1
Jesús Alberola Herrero <jesusalberola90@gmail.com>
2025-02-26 14:22:10 +0100

    refactor single product page

--- src/pages/SingleProduct/Comments/Comments.component.tsx
+++ src/pages/SingleProduct/Comments/Comments.component.tsx
@@ -3,12 +3,10 @@ import SingleComment from "./components/SingleComment/SingleComment.component";
 import { ReviewsArray } from "../../../lib/schemas/productSchema";
 import { formatRating } from "./Comments.utils";
 import Button from "../../../components/Button/Button.component";
+import { getAverageRating } from "../../../utils/averageRating";
+import NoComments from "./components/NoComments/NoComments.component";
 
 export default function Comments({ reviews }: { reviews: ReviewsArray }) {
-  const totalRating = reviews.length
-    ? reviews.reduce((acc, review) => acc + review.rating, 0) / reviews.length
-    : 0;
-
   return (
     <>
       <div className="flex flex-col gap-5 sm:gap-0 sm:flex-row items-center justify-between border-t border-neutral-300 py-10">
@@ -19,7 +17,7 @@ export default function Comments({ reviews }: { reviews: ReviewsArray }) {
 
           <div className="flex gap-2 items-center mt-1">
             <div className="flex  items-center">
-              <RatingStars size={24} rating={totalRating} />
+              <RatingStars size={24} rating={getAverageRating(reviews)} />
             </div>
             <span className="text-neutral-500 font-sans text-sm font-normal">
               {reviews.length > 0 ? formatRating(reviews) : 0} out of 5
@@ -32,23 +30,11 @@ export default function Comments({ reviews }: { reviews: ReviewsArray }) {
       </div>
 
       {reviews.length === 0 ? (
-        <div className="border-t border-neutral-300 py-32 w-full text-center">
-          <p className="text-neutral-500 font-condensed uppercase font-normal text-sm">
-            No reviews yet
-          </p>
-        </div>
+        <NoComments />
       ) : (
         reviews.map((review) => (
           <SingleComment key={review.id + Math.random()} review={review} />
         ))
-      )}
-
-      {reviews.length > 5 && (
-        <div className="max-w-[120px] mx-auto mt-8">
-          <button className="flex justify-center items-center w-full bg-neutral-950 text-neutral-50 font-condensed tracking-widest uppercase text-sm font-lighter py-3 cursor-pointer">
-            Read more
-          </button>
-        </div>
       )}
     </>
   );
---
+++ src/pages/SingleProduct/Comments/components/NoComments/NoComments.component.tsx
@@ -0,0 +1,9 @@
+export default function NoComments() {
+  return (
+    <div className="border-t border-neutral-300 py-32 w-full text-center">
+      <p className="text-neutral-500 font-condensed uppercase font-normal text-sm">
+        No reviews yet
+      </p>
+    </div>
+  );
+}
