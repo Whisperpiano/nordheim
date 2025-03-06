9d9f3863b885e720f02981e154e306c572ab015c
Whisperpiano <jesusalberola90@gmail.com>
2025-03-06 10:03:47 +0100

    added background pulse to product images when loading

--- src/components/ProductCard/components/ImageProduct/ImageProduct.component.tsx
+++ src/components/ProductCard/components/ImageProduct/ImageProduct.component.tsx
@@ -1,9 +1,11 @@
-import { forwardRef, useEffect } from "react";
+import { forwardRef, useEffect, useState } from "react";
 import { cn } from "../../../../utils/cn";
 import { ImageProductProps } from "./ImageProduct.types";
 
 const ImageProduct = forwardRef<HTMLImageElement, ImageProductProps>(
   ({ category, slug, className, format = "both", ...props }, ref) => {
+    const [isLoading, setIsLoading] = useState(true);
+
     // Preload image in browser, so it loads faster in single product page and in cart
     useEffect(() => {
       const preloadImageXL = new Image();
@@ -13,41 +15,49 @@ const ImageProduct = forwardRef<HTMLImageElement, ImageProductProps>(
     }, [category, slug]);
 
     return (
-      <picture>
-        {/* AVIF */}
-        {format === "avif" || format === "both" ? (
-          <source
-            srcSet={`
+      <div
+        className={cn(
+          "relative w-full h-full",
+          isLoading && "bg-neutral-500/25 animate-pulse"
+        )}
+      >
+        <picture>
+          {/* AVIF */}
+          {format === "avif" || format === "both" ? (
+            <source
+              srcSet={`
                 https://ppufwgcofnfrgdeqxesf.supabase.co/storage/v1/object/public/${category}-images/${slug}-md.avif 640w, 
                 https://ppufwgcofnfrgdeqxesf.supabase.co/storage/v1/object/public/${category}-images/${slug}-sm.avif 360w
               `}
-            sizes="(min-width: 1024px) 640px, 360px"
-            type="image/avif"
-          />
-        ) : null}
+              sizes="(min-width: 1024px) 640px, 360px"
+              type="image/avif"
+            />
+          ) : null}
 
-        {/* WebP */}
-        {format === "webp" || format === "both" ? (
-          <source
-            srcSet={`
+          {/* WebP */}
+          {format === "webp" || format === "both" ? (
+            <source
+              srcSet={`
                 https://ppufwgcofnfrgdeqxesf.supabase.co/storage/v1/object/public/${category}-images/${slug}-md.webp 640w, 
                 https://ppufwgcofnfrgdeqxesf.supabase.co/storage/v1/object/public/${category}-images/${slug}-sm.webp 360w
               `}
-            sizes="(min-width: 1024px) 640px, 360px"
-            type="image/webp"
-          />
-        ) : null}
+              sizes="(min-width: 1024px) 640px, 360px"
+              type="image/webp"
+            />
+          ) : null}
 
-        {/* Fallback JPG */}
-        <img
-          ref={ref}
-          src={`https://ppufwgcofnfrgdeqxesf.supabase.co/storage/v1/object/public/${category}-images/${slug}.jpg`}
-          alt={`Home image of ${slug}`}
-          sizes="(min-width: 1024px) 640px, 360px"
-          className={cn(className)}
-          {...props}
-        />
-      </picture>
+          {/* Fallback JPG */}
+          <img
+            ref={ref}
+            src={`https://ppufwgcofnfrgdeqxesf.supabase.co/storage/v1/object/public/${category}-images/${slug}.jpg`}
+            alt={`Home image of ${slug}`}
+            sizes="(min-width: 1024px) 640px, 360px"
+            className={cn(className)}
+            onLoad={() => setIsLoading(false)}
+            {...props}
+          />
+        </picture>
+      </div>
     );
   }
 );
